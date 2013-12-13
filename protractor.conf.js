395a64f3cbc444d59443b4acdc03263b01796a44
Mackenzie Browne <mackbrowne@gmail.com>
2013-12-12 19:05:05 -0500

    added e2e tests with protractor

---
+++ protractor.conf.js
@@ -0,0 +1,5 @@
+// myConf.js
+exports.config = {
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['test/e2e/**/*.js']
+}
---
+++ test/e2e/user.js
@@ -0,0 +1,18 @@
+// myTest.js
+describe('angularjs homepage', function() {
+  it('should greet the named user', function() {
+    browser.get('http://localhost:3001');
+
+   // element(by.model('yourName')).sendKeys('Julie');
+    element(by.linkText('Signup')).click();
+    element(by.id('name')).sendKeys('TestUser1');
+    element(by.id('email')).sendKeys('TestUser1@test.com');
+    element(by.id('username')).sendKeys('testusr1');
+    element(by.id('password')).sendKeys('testusr1');
+    element(by.className('btn-default')).click();
+    expect(browser.getCurrentUrl()).toEqual('http://localhost:3001/#/')
+    //var greeting = element(by.binding('yourName'));
+
+  //  expect(greeting.getText()).toEqual('Hello Julie!');
+  }, 10000);
+});
